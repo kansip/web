dca68f07e3c59206726578c444ff884f3529c78a
Kirill <kirik200088@gmail.com>
2020-03-24 16:33:57 +0300

    change

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,11 +18,10 @@ from django.urls import path
 from qa import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.test),
     path('login', views.test),
     path('siqnup', views.test),
-    path(r'question/<int:id>', views.test),
+    path('question/<int:id>', views.test),
     path('ask', views.test),
     path('popular', views.test),
     path('new', views.test)
